8224c341d3a47f14cb3a8268fbcc17a216aeff06
Jonathan Krall Olsson <jonathan.krall@lexicon.se>
2024-02-28 14:05:18 +0100

    Part one, Classes and Attributes with Droids

---
+++ EmployeeList/Droid.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeList
+{
+    internal class Droid
+    {
+        //Fields. Privata, kommer inte åt utanför klassen. Liten bokstav
+        private string firstname;
+
+        private int salary; 
+
+        //Property, publikt går att komma åt utanför klassen. Stor bokstav
+        public int Salary
+        {
+            get { return salary; }
+            set
+            {
+                if(value < 0)
+                {
+                    salary = 0;
+                }
+                else
+                {
+                    salary = value;
+                }
+            }
+        }
+        //Auto property. 
+        public int Salary2 { get; set ; }
+
+        //private int salary2; 
+        //public int Salary2
+        //{
+        //    get { return salary2; }
+        //    set { salary2 = value; }
+        //}
+
+
+
+        public Droid(string name)
+        {
+            firstname = name;
+        }
+
+        public string GetFirstName()
+        {
+            return firstname;
+        }
+        public void SetFirstName(string name)
+        {
+            firstname = name; 
+        }
+    }
+}
--- EmployeeList/Program.cs
+++ EmployeeList/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Droid d = new Droid("C-3PO");
+
+            Console.WriteLine(d.GetFirstName());
+
+            //d.firstname = "R2-D2";
+            d.SetFirstName("R2-D2");
+            Console.WriteLine(d.GetFirstName());
+            d.Salary = 20000; 
+            
         }
     }
 }
