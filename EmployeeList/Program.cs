ad7a39c4a9fe6e0d3d211451d4a910225a00b627
Jonathan Krall Olsson <jonathan.krall@lexicon.se>
2024-02-28 14:36:55 +0100

    Employee och Payroll implemented

--- EmployeeList/Program.cs
+++ EmployeeList/Program.cs
@@ -4,15 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Droid d = new Droid("C-3PO");
+            //Droid d = new Droid("C-3PO");
 
-            Console.WriteLine(d.GetFirstName());
+            //Console.WriteLine(d.GetFirstName());
 
-            //d.firstname = "R2-D2";
-            d.SetFirstName("R2-D2");
-            Console.WriteLine(d.GetFirstName());
-            d.Salary = 20000; 
-            
+            ////d.firstname = "R2-D2";
+            //d.SetFirstName("R2-D2");
+            //Console.WriteLine(d.GetFirstName());
+            //d.Salary = 20000; 
+
+            Employee emp = new Employee("Anders", 20000);
+
+            Console.WriteLine($"Name: {emp.Name} Salary: {emp.Salary}");
+
+            emp.Name = "Nisse";
+
+            Console.WriteLine($"Name: {emp.Name} Salary: {emp.Salary}");
+
         }
     }
 }
